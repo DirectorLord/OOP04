1702b51ac9ce4dfa92122ab07b21ef5d0544152d
DirectorLord <thelord161203@gmail.com>
2025-07-16 08:41:01 +0300

    Add project files.

---
+++ OOP04/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace OOP04
+{
+    public class Program
+    {
+        #region Part01
+        #region Question01
+
+        #endregion
+
+        #region Question02
+
+        #endregion
+
+        #region Question03
+
+        #endregion
+
+        #region Question04
+
+        #endregion
+
+        #region Question05
+
+        #endregion
+
+        #region Question06
+
+        #endregion
+
+        #region Question07
+
+        #endregion
+
+        #region Question08
+
+        #endregion
+
+        #region Question09
+
+        #endregion
+
+        #region Question010
+
+        #endregion
+        #endregion
+
+        #region Part02
+
+        #region Question01
+
+        #endregion
+
+        #region Question02
+
+        #endregion
+
+        #region Question03
+
+        #endregion
+        #endregion
+    }
+}
